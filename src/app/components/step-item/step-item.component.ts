9d3b4c00cda4f88f8d1f6df9d47f8dac62cddbc1
avivsbt <avivsbt@gmail.com>
2024-03-07 20:16:53 +0200

    [UI] step item

--- src/app/components/step-item/step-item.component.ts
+++ src/app/components/step-item/step-item.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-step-item',
+  selector: 'step-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './step-item.component.html',
   styleUrl: './step-item.component.css'
 })
 export class StepItemComponent {
 
+  @Input() public num: number = 1;
+  @Input() public text: string = "";
+  @Input() public information: boolean = false;
+  @Input() public selected: number = 1;
+  @Output() public selectedEvent = new EventEmitter<number>();
+
+  onSelected(): void {
+    this.selectedEvent.emit(this.num);
+  }
 }
--- src/app/components/wrapper-steps/wrapper-steps.component.ts
+++ src/app/components/wrapper-steps/wrapper-steps.component.ts
@@ -1,12 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { StepItemComponent } from '../step-item/step-item.component';
 
 @Component({
   selector: 'wrapper-steps',
   standalone: true,
-  imports: [],
+  imports: [StepItemComponent],
   templateUrl: './wrapper-steps.component.html',
   styleUrl: './wrapper-steps.component.css',
 })
 export class WrapperStepsComponent {
   @Input() public position: 'top' | 'left' = 'left';
+
+  onSelected(selected: number): void {
+    console.log(selected);
+  }
+  
 }
