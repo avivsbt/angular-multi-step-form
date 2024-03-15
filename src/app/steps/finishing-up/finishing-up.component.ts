85e7645ff8a9e4610eb8a2d190df4571685b09f6
avivsbt <avivsbt@gmail.com>
2024-03-15 16:12:28 +0200

    [FinishingUpComponent] BL

--- src/app/steps/finishing-up/finishing-up.component.ts
+++ src/app/steps/finishing-up/finishing-up.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, computed, input } from '@angular/core';
+import { Component, Input, computed, inject, input } from '@angular/core';
 import { DescriptionStepComponent } from '../../components/description-step/description-step.component';
 import { WrapperButtonsComponent } from '../../components/wrapper-buttons/wrapper-buttons.component';
 import { FormButtonComponent } from '../../components/form-button/form-button.component';
 import { JsonPipe } from '@angular/common';
-import { FormValue } from '../../types';
+import { ESteps, FormValue, IPlan, IaddOns } from '../../types';
+import { MultiStepFormService } from '../../services/multi-step-form.service';
+import { addOnses, plans } from '../../shared/data/data';
 
 @Component({
   selector: 'finishing-up',
@@ -20,6 +22,27 @@ import { FormValue } from '../../types';
 
 
 export class FinishingUpComponent {
-  public formValue = input<FormValue>();
+  public multiStepFormService = inject(MultiStepFormService);
+  public formValue = input<FormValue | any>();
   @Input({ required: true }) public disabled: boolean = true;
+
+  public plan = computed(() => plans.find(plan => plan.value === this.formValue()?.plan?.plan) as IPlan);
+
+  public filteredAddOns = computed(() => addOnses.filter(addOn => this.formValue().addOns[addOn.formControlName]));
+
+  public total = computed(() => {
+    let planSum: number = this.multiStepFormService.isYearly() ? this.plan()?.yearly : this.plan()?.monthly;
+    
+    let addonsSum: number = this.filteredAddOns().reduce((acc, obj) => {
+      let price = this.multiStepFormService.isYearly() ? obj.yearly : obj.monthly
+      return acc + price
+    }, 0);
+
+    return planSum + addonsSum
+  });
+
+  onChangePlan(): void {
+    this.multiStepFormService.setStep(ESteps.Plan);
+  }
+
 }
